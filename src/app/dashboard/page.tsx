009c68c7431f71adec7e80ddad7a229ae960062e
Malvis <malvisanochie97@gmail.com>
2025-02-27 01:28:25 +0200

    first commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,20 @@
+
+// const DashboardPage: React.FC = () => {
+//     return (
+//         <div>
+//             <h1>Welcome Mal</h1>
+//         </div>
+//     );
+// };
+
+// export default DashboardPage;
+
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
