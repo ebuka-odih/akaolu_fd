7e790d6e2e3a2e229599cee08cfa67501c069015
Malvis <malvisanochie97@gmail.com>
2025-03-08 12:06:01 +0100

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -27,7 +28,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-           <ThemeProvider
+        
+          <ThemeProvider
             attribute="class"
             defaultTheme="system"
             enableSystem
